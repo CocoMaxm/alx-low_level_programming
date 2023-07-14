f20e578c4a1ab27afda7803a82bb4e616160eb8b
MasagoM <masagomax@gmail.com>
2023-07-14 13:36:35 -0700

    0 ups

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,19 +5,19 @@
  */
 int main(void)
 {
-	int n;
+	int n = 10;
 
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive", n);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is negative", n);
 	}
 	else
 	{
-		printf("%d is negative", n);
+		printf("%d is zero", n);
 	}
 	return (0);
 }
