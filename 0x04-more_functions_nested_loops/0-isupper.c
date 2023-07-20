93cbecf39b05c3d6fa636ad4f722983c69dd7f6c
MasagoM <masagomax@gmail.com>
2023-07-20 16:17:58 -0700

    C Mandatory

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/*
+ * _isupper - checks for uppercase
+ * @c: number to be checked
+ *
+ * Return: 1 = uppercase 0 = other
+ */
 int _isupper(int c)
 {
-	if
+	if (c >= 65 && c <=90)
 	{
-		(c >= 65 && <=90);
-		
 		return (1);
 	}
+
 	return (0);
 }
