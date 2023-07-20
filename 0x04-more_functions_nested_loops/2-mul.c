f74433ac6bbf54590d525017412c2d549cc3149b
MasagoM <masagomax@gmail.com>
2023-07-20 16:07:39 -0700

    C Mandatory

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * mul - main
- * @int a - first value
- * @int b - second value
+ * mul - multiplication
+ * @int a: first value
+ * @int b: second value
+ *
  * Return: always 0
  */
-int mul(int a,int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
